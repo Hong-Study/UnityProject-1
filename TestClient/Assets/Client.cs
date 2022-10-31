f7841392a3e6f80836e0f8bd9518f81bc33e3c11
HongJiHyun <fnvl7855@naver.com>
2022-10-31 23:50:17 +0900

    기초 Send Recv

--- TestClient/Assets/Client.cs
+++ TestClient/Assets/Client.cs
@@ -10,5 +10,8 @@ public class Client : MonoBehaviour
     {
         clientInterface = new ClientInterface();
         clientInterface.Start();
+
+        if (clientInterface.isRead())
+            clientInterface.Read();
     }
 }
